613e0fe05a9046c19c2fc947cda42ff8d017d78f
Brys <brysontenhaken@gmail.com>
2021-07-18 18:06:03 -0500

    Update VersionManager.kt

--- src/main/kotlin/org/feuer/partners/zuxces/core/VersionManager.kt
+++ src/main/kotlin/org/feuer/partners/zuxces/core/VersionManager.kt
@@ -3,6 +3,6 @@ package org.feuer.partners.zuxces.core
 import net.dv8tion.jda.api.JDAInfo
 
 object VersionManager {
-    val zuxcesUtilities = "1.0.007"
+    val zuxcesUtilities = "1.0.01"
     val JDA = JDAInfo.VERSION
 }
